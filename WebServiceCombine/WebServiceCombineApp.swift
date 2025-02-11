c20e94f5ca0a8ef3515c37e20c64a038aff63963
Fatih <fatihk.96brs@gmail.com>
2025-02-12 01:17:56 +0300

    Initial Commit

---
+++ WebServiceCombine/WebServiceCombineApp.swift
@@ -0,0 +1,17 @@
+//
+//  WebServiceCombineApp.swift
+//  WebServiceCombine
+//
+//  Created by Fatih Kenarda on 12.02.2025.
+//
+
+import SwiftUI
+
+@main
+struct WebServiceCombineApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
